f63520ef557f76262e504d55dd9d40d72542d2b3
Sean Yoon <seanyoonsy@gmail.com>
2022-07-04 07:08:34 -0700

    Add result variable and use if, else if, else conditional statements to get rock, paper, scissors outputs

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(num) {
+    const result = Math.floor(Math.random() * num);
+    if (result === 0) {
+        console.log("Rock");
+    }   else if (result === 1) {
+        console.log("Paper");
+    }   else {
+        console.log ("Scissors");
+    }
 }
 
 const computerPlay = getRandomInt (3);
