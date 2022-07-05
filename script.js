454b6b950f3cfa2072dce528f8f995b6ba8a7aa0
Sean Yoon <seanyoonsy@gmail.com>
2022-07-04 22:31:15 -0700

    Create playRound function with user prompt and if, else, else if conditional statements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computerPlay(num = 3) {
     result = Math.floor(Math.random() * num);
-    
+
     if (result === 0) {
         console.log("Rock");
     }   else if (result === 1) {
@@ -10,4 +10,16 @@ function computerPlay(num = 3) {
     }
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+
+    prompt ("Rock, Paper or Scissors?", "");
+
+    if (playerSelection === "rock" && selection === answer.toLowerCase()) {
+        return playerSelection;
+    }   else if (playerSelection === "paper" && selection === answer.toLowerCase()) {
+        return playerSelection;
+    }   else (playerSelection === "scissors" && selection === answer.toLowerCase()) {
+        return playerSelection;
+    }
+    
+}
