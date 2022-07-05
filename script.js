f849f8c0d8b88bde20c5d0b1901410b74ce29e80
Sean Yoon <seanyoonsy@gmail.com>
2022-07-05 01:29:30 -0700

    Create function game() with loop to play 5 games, add score variables with increment operators to playRound function

--- script.js
+++ script.js
@@ -14,17 +14,35 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         return `It's a tie! You both picked ${playerSelection}`;
+        tieScore++;
     }   else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats Scissors";
+        playerScore++;
     }   else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats Rock";
+        playerScore++;
     }   else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors beats Paper";
+        playerScore++;
     }   else {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
     }
 }
+
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
 const playerSelection = prompt ("Rock, Paper or Scissors?").toLowerCase();
 
 const computerSelection = computerPlay(num = 3);
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        console.log (playRound (playerSelection, computerSelection));
+    }
+}
+
+game ();
