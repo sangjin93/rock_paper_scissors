05417a7a8a5b5fec77ecc4c6b647174029e430dd
Sean Yoon <seanyoonsy@gmail.com>
2022-07-10 23:40:54 -0700

    Change button class to id and create event listeners for the buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const buttons = document.querySelectorAll('.choice');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        return button.id;
+    });
+});
+
 // Function that randomly determines the computer's play
 function computerPlay(num = 3) {
     result = Math.floor(Math.random() * num);
